5032e128ef413e761c4d65d080ab1abd82bb8fac
SkyH1ghDev <developer.skyh1gh@gmail.com>
2025-11-21 09:07:22 +0100

    refactored libdirs for external

--- Application/premake5.lua
+++ Application/premake5.lua
@@ -3,13 +3,14 @@ project "Application"
     kind "ConsoleApp"
     location(rootPath .. "/Application")
 
+    local externalLibPath = targetBuildPath .. "/External/lib"
+    libdirs(externalLibPath)
     targetdir(targetBuildPath .. "/%{prj.name}")
     objdir(objBuildPath .. "/%{prj.name}")
     files {"inc/**.hpp", "src/**.cpp"}
     includedirs{"../Library/include", targetBuildPath .. "/External/include" , "inc"}
     dependson{"ImGui", "SDL3"}
 
-    local externalLibPath = targetBuildPath .. "/External/lib"
     links{"Library",
-        externalLibPath .. "/ImGui",
-        externalLibPath .. "/SDL3"}
+        "/ImGui",
+        "/SDL3"}
