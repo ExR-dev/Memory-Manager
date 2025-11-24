a44d01f0c0910cb4fd05fd6cb8648e97fc999b31
Edvin Roth <99540868+ExR-dev@users.noreply.github.com>
2025-11-24 11:50:33 +0100

    Almost finished pool allocation, working on tests

--- Temp/TempProj/src/main.cpp
+++ Temp/TempProj/src/main.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
-
+#include "../inc/PageRegistry.hpp"
 
 int main()
 {
-    // TODO
+	using namespace MemoryInternal;
+
+	std::cout << "Test 1: Allocate and free integers" << std::endl;
+	{
+		int *allocedInts = PageRegistry<int>::Alloc(10);
+
+		for (int i = 0; i < 10; ++i)
+			allocedInts[i] = i * 10;
+
+		std::cout << "Allocated integers: ";
+		for (int i = 0; i < 10; ++i)
+			std::cout << allocedInts[i] << " ";
+		std::cout << std::endl;
+
+		PageRegistry<int>::Free(allocedInts, 10);
+	}
+
+	std::cout << "Test 2: Re-allocate same space" << std::endl;
+	{
+		int *allocedInts = PageRegistry<int>::Alloc(10);
+
+		for (int i = 0; i < 10; ++i)
+			allocedInts[i] = i * 10;
+
+		std::cout << "Allocated integers: ";
+		for (int i = 0; i < 10; ++i)
+			std::cout << allocedInts[i] << " ";
+		std::cout << std::endl;
+
+		PageRegistry<int>::Free(allocedInts, 10);
+	}
+
+	std::cout << "Test 3: Multiple allocations" << std::endl;
+	{
+		int *allocArray[6]{};
+		int allocSizes[6] = { 5, 10, 18, 2, 1, 64 };
+		
+		for (int i = 0; i < 6; ++i)
+		{
+			allocArray[i] = PageRegistry<int>::Alloc(allocSizes[i]);
+
+			for (int j = 0; j < allocSizes[i]; ++j)
+				allocArray[i][j] = i * 100 + j;
+		}
+
+		for (int i = 0; i < 6; ++i)
+		{
+			std::cout << "Allocated block " << i << ": ";
+			for (int j = 0; j < allocSizes[i]; ++j)
+				std::cout << allocArray[i][j] << " ";
+			std::cout << std::endl;
+		}
+
+		for (int i = 0; i < 6; ++i)
+		{
+			PageRegistry<int>::Free(allocArray[i], allocSizes[i]);
+		}
+	}
 
     return 0;
 }
