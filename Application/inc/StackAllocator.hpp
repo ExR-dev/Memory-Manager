b1310838bac1b99a72057df1d401df1c0b4a7d43
Linus Holmström <linus.holmstrom@hotmail.com>
2025-11-25 10:33:08 +0100

    Changed StackAllocator to be header-only.

--- Application/inc/StackAllocator.hpp
+++ Application/inc/StackAllocator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <memory>
 #include <array>
+#include <iostream>
 
 #define STACK_SIZE 1 << 14
 typedef std::unique_ptr<std::array<char, STACK_SIZE>> StorageType;
@@ -8,16 +9,54 @@ typedef std::unique_ptr<std::array<char, STACK_SIZE>> StorageType;
 class StackAllocator
 {
 private:
-	StorageType m_stack = {};
+	StorageType m_stack;
 	size_t m_top = 0;
 
 public:
-	StackAllocator();
+	StackAllocator()
+	{
+		m_stack = std::make_unique<std::array<char, STACK_SIZE>>();
+		*(m_stack.get()) = {};
+	}
 
-	void* At(size_t idx);
-	size_t Push(void* data, size_t size);
-	void Reset();
+	void* At(size_t idx)
+	{
+		if (idx >= m_top)
+			return nullptr;
 
-	size_t DBG_GetTop();
-	StorageType& DBG_GetStack();
+		return &m_stack.get()->at(idx);
+	}
+
+	size_t Push(void* data, size_t size)
+	{
+		if (m_top + size > STACK_SIZE)
+			return (size_t)-1;
+
+		size_t start = m_top;
+		char* begin = m_stack.get()->data();
+
+		for (size_t i = 0; i < size; i++)
+		{
+			char* foo = (char*)data;
+			begin[m_top] = foo[i];
+			m_top++;
+		}
+
+		return start;
+	}
+
+	void Reset()
+	{
+		m_top = 0;
+	}
+
+	size_t DBG_GetTop()
+	{
+		return m_top;
+	}
+
+	StorageType& DBG_GetStack()
+	{
+		return m_stack;
+	}
 };
